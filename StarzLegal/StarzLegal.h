cd05a4af1ad1779b72924d97bc8c9e8a08cf8d24
Anthony Picciano <picciano1@anthonys-macbook-pro.local>
2015-11-09 11:22:06 -0700

    Initial Commit

---
+++ StarzLegal/StarzLegal.h
@@ -0,0 +1,19 @@
+//
+//  StarzLegal.h
+//  StarzLegal
+//
+//  Created by Anthony Picciano on 11/9/15.
+//  Copyright © 2015 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StarzLegal.
+FOUNDATION_EXPORT double StarzLegalVersionNumber;
+
+//! Project version string for StarzLegal.
+FOUNDATION_EXPORT const unsigned char StarzLegalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StarzLegal/PublicHeader.h>
+
+
---
+++ StarzLegalTests/StarzLegalTests.swift
@@ -0,0 +1,36 @@
+//
+//  StarzLegalTests.swift
+//  StarzLegalTests
+//
+//  Created by Anthony Picciano on 11/9/15.
+//  Copyright © 2015 Anthony Picciano. All rights reserved.
+//
+
+import XCTest
+@testable import StarzLegal
+
+class StarzLegalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
